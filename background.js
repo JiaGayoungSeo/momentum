3c3e83683d7a0cadfd0cc57731070eaf5de5aff0
Seo <seo8556@saskpolytech.ca>
2019-01-30 21:53:13 -0600

    Momentum

---
+++ background.js
@@ -0,0 +1,25 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 6;
+
+
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber+1}.jpg`
+    image.classList.add("bgImage");
+    body.prepend(image);
+
+}
+
+function genRandon() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandon();
+    paintImage(randomNumber)
+}
+
+init();
